04ec0a39398c6a251dfe0c988049dad770dc2a7f
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-06-30 17:34:11 -0300

    inserido uma sessão para cada filme
    closes #9

---
+++ InserirSessao.sql
@@ -0,0 +1,3 @@
+insert into sessao(Horario_Sessao, Sala_Sessao, Preco_Sessao, Assento_Sessao, Filme_idFilme)
+values ('21:15:00', 1, 22, 12, 1), ('21:15:00', 2, 22, 16, 2), ('21:15:00', 3, 22, 63, 3);
+

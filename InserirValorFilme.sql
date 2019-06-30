6c6969b8a6ce4bd73c7d58118d5ce66873062851
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-06-30 17:33:00 -0300

    Inserindo 3 filmes
    closes #8

---
+++ InserirValorFilme.sql
@@ -0,0 +1,5 @@
+insert into filme (Titulo_Filme, Descricao_Filme, Elenco_Filme, FaixaEtaria_Filme, Duracao_Filme, Distribuidora)
+values ("Piratas do caribe 58", "meu jesus eles realmente nao cansam de fazer esse filme o jhonny dep ta velho já", "Jhonny deep", 18, 128, "Disney")
+,  ("star wars 21", "o quilo rei continua sendo o pior vilão de todos os tempos", "Ben swolo", 18, 240, "Disney"),
+ ("need for speed 11", "Agora eles estão resolvendo um problema no espaço e correndo em foguetes", "Vin Etanol", 18, 60, "Fox");
+

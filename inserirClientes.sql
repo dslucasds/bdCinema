e2836a5b99a54c4c4a946029214df4bb5d6b7204
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-06-30 17:31:10 -0300

    inserindo clientes
    closes #7

---
+++ inserirClientes.sql
@@ -0,0 +1,8 @@
+SELECT * FROM cinema.cliente;
+insert into cliente (Nom_Cliente, Usuario_Cliente, 
+Senha_Cliente, Email_Cliente, Cpf_Cliente, Cep_Cliente, DataNasc_Cliente,
+Sexo_Cliente, Telefone_Cliente, Estado_Cliente, Cidade_Cliente, Ender_Cliente, 
+Bairro_Cliente, Num_Cliente) 
+values ('Deivid','Deivid', 123, 'deivid@gmail.com', 09881999895 , 88360000, '1994-11-29', 'masculino', 47992168342, 'SC', 'Brusque', 'Guarani', 'Guarani', 690 ),
+('Lucas', 'Lucas', 456, 'lucas@gmail.com', 07188208970, 8836000, '2000-07-08', 'masculino', 47996276422, 'SC', 'Brusque', 'Azambuja', 'AZ 41', 255),
+('Leo', 'Leo', 789, 'leo@gmail.com', 10545847969, 88360000, '1999-09-20', 'masculino', 47991558700, 'SC', 'Guabiruba', 'Rua Lorena', 'São Pedro', 1757);

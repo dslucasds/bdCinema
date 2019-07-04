c40e8926ae638dce8a2ef8f0356b55597e0e0480
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-07-03 21:46:16 -0300

    arrumar mais um erro

--- criarTabelaCartaoCredito.sql
+++ criarTabelaCartaoCredito.sql
@@ -2,6 +2,6 @@
  idCartao_Credito INT NOT NULL AUTO_INCREMENT,
  Nome_Cartao VARCHAR(45),
  dataValidade_Cartao DATE,
- NumCartao_Cartao INT(16),
+ NumCartao_Cartao float(16),
  Cliente_idCliente INT NOT NULL,
  PRIMARY KEY (idCartao_Credito));

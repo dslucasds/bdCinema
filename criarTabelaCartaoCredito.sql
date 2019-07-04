138c070458be006bd0ef1a950a8d3d0da2f05d4b
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-07-03 21:39:59 -0300

    Arrumar dados

--- criarTabelaCartaoCredito.sql
+++ criarTabelaCartaoCredito.sql
@@ -4,8 +4,4 @@
  dataValidade_Cartao DATE,
  NumCartao_Cartao INT(16),
  Cliente_idCliente INT NOT NULL,
- PRIMARY KEY (idCartao_Credito, Cliente_idCliente),
- INDEX fk_Cartao_Credito_Cliente1_idx (Cliente_idCliente ASC),
- CONSTRAINT fk_Cartao_Credito_Cliente1
-   FOREIGN KEY (Cliente_idCliente)
-   REFERENCES Cliente (idCliente));
+ PRIMARY KEY (idCartao_Credito));

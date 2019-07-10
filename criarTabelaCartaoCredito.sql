49c3088aa39898ec8db155d1d00098bf252f27ec
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-07-10 19:20:44 -0300

    inserir tabela cartao
    arrumar mais um erro

--- criarTabelaCartaoCredito.sql
+++ criarTabelaCartaoCredito.sql
@@ -1,7 +1,7 @@
- CREATE TABLE IF NOT EXISTS cartaoCredito(
+  CREATE TABLE IF NOT EXISTS cartaoCredito(
  idCartao_Credito INT NOT NULL AUTO_INCREMENT,
  Nome_Cartao VARCHAR(45),
  dataValidade_Cartao DATE,
- NumCartao_Cartao float(16),
+ NumCartao_Cartao varchar(16),
  Cliente_idCliente INT NOT NULL,
  PRIMARY KEY (idCartao_Credito));

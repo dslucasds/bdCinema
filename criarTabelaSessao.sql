6cad2c9b42a3652b33ecf190f2596edb10f692c9
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-07-10 19:11:51 -0300

    ajustes na sessão

--- criarTabelaSessao.sql
+++ criarTabelaSessao.sql
@@ -1,12 +1,8 @@
-CREATE TABLE IF NOT EXISTS Sessao (
+ CREATE TABLE IF NOT EXISTS Sessao (
  idSessao INT NOT NULL AUTO_INCREMENT,
  Horario_Sessao TIME NULL,
  Sala_Sessao INT(3) NULL,
  Preco_Sessao FLOAT(3) NULL,
  Assento_Sessao INT(3) NULL,
  Filme_idFilme INT NOT NULL,
- PRIMARY KEY (idSessao, Filme_idFilme),
- INDEX fk_Sessao_Filme_idx (Filme_idFilme ASC),
- CONSTRAINT fk_Sessão_Filme
-   FOREIGN KEY (Filme_idFilme)
-   REFERENCES Filme (idFilme));
+ PRIMARY KEY (idSessao, Filme_idFilme));

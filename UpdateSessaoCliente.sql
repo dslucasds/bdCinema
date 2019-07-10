21ddab37919e961dccd1c3243de9497768f76927
dslucasds <45804463+dslucasds@users.noreply.github.com>
2019-07-10 19:59:53 -0300

    Add Valores nos Assentos
    closes #18

---
+++ UpdateSessaoCliente.sql
@@ -0,0 +1,14 @@
+
+UPDATE cliente_sessao
+SET Assento_Sessao = 13
+WHERE Cliente_idCliente = 1;
+
+
+UPDATE cliente_sessao
+SET Assento_Sessao = 17
+WHERE Cliente_idCliente = 2;
+
+
+UPDATE cliente_sessao
+SET Assento_Sessao = 69
+WHERE Cliente_idCliente = 3;
